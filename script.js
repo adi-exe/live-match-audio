88cdc501a8e0bb649e0a8604e77619cf8ef8dfef
Aditya Dhanraj <134140404+adi-exe@users.noreply.github.com>
2023-11-19 13:30:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 window.onload = function() {
-    var audio = document.getElementById('myAudio');
+  var audio = document.getElementById('myAudio');
+  var playButton = document.getElementById('playButton');
+
+  playButton.addEventListener('click', function() {
     audio.play();
+  });
 };
